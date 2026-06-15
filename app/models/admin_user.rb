85ecb1955201430d44bf1ec268ac063235023169
Usama <usama.zubair@snappymob.com>
2026-06-15 08:50:50 +0800

    removing confirmed at

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -25,7 +25,7 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   def self.ransackable_attributes(auth_object = nil)
     [
