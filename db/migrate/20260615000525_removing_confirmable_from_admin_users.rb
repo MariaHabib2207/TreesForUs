236e48f1207940ceda3c107b28a8faca20cb4557
Usama <usama.zubair@snappymob.com>
2026-06-15 08:15:29 +0800

    adding letter-opener and removing confirmable

--- db/migrate/20260615000525_removing_confirmable_from_admin_users.rb
+++ db/migrate/20260615000525_removing_confirmable_from_admin_users.rb
@@ -1,5 +1,6 @@
 class RemovingConfirmableFromAdminUsers < ActiveRecord::Migration[8.0]
-  
+  def up
+
     remove_column :admin_users, :confirmed_at
     remove_column :admin_users, :confirmation_sent_at
     remove_column :admin_users, :unconfirmed_email
@@ -10,7 +11,5 @@ class RemovingConfirmableFromAdminUsers < ActiveRecord::Migration[8.0]
     add_column :admin_users, :confirmed_at,         :datetime
     add_column :admin_users, :confirmation_sent_at, :datetime
     add_column :admin_users, :unconfirmed_email,    :string
-
-
   end
 end
