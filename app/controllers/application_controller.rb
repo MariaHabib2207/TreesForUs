8642692a44b3a9b89c4edb877f75d743046bf537
Usama <usama.zubair@snappymob.com>
2026-06-26 08:44:59 +0800

    manifest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def index
     if user_signed_in?
-      redirect_to dashboard_path
+      redirect_to authenticated_root_path
     else
       redirect_to new_user_session_path
     end
