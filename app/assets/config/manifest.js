1b8f58931fa920bedbb526132105cb6468cf1d28
Usama <usama.zubair@snappymob.com>
2026-06-26 08:23:12 +0800

    fixing manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .js
 //= link life_activities.css
+ //= link logo.png
+ //= link favicon.ico
+ //= link chatroom.css
+ //= link family_tree.css
