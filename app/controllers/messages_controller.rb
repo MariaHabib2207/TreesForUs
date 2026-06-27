068f6a1d9f8587fb854a7684c92b46a880e982d3
Usama <usama.zubair@snappymob.com>
2026-06-27 08:11:01 +0800

    Fix message bubble sides with per-user broadcast

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,35 +1,37 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
 
-def create
-  @chatroom = Chatroom.joins(:chatroom_members)
-                      .where(chatroom_members: { user_id: current_user.id })
-                      .find(params[:chatroom_id])
+  def create
+    @chatroom = Chatroom.joins(:chatroom_members)
+                        .where(chatroom_members: { user_id: current_user.id })
+                        .find(params[:chatroom_id])
 
-  message_params = params[:message] || params
-  
-  @message = @chatroom.messages.build(
-    user: current_user,
-    body: message_params[:body]
-  )
+    message_params = params[:message] || params
 
-  if message_params[:attachments].present?
-    @message.attachments.attach(message_params[:attachments])
-  end
-
-  if @message.save
-    ChatroomChannel.broadcast_to(
-      @chatroom,
-      {
-        message_html: render_to_string(
-          partial: "messages/message",
-          locals: { message: @message, current_user: current_user }
-        )
-      }
+    @message = @chatroom.messages.build(
+      user: current_user,
+      body: message_params[:body]
     )
-    head :ok
-  else
-    head :unprocessable_entity
+
+    if message_params[:attachments].present?
+      @message.attachments.attach(message_params[:attachments])
+    end
+
+    if @message.save
+      @chatroom.members.each do |member|
+        ActionCable.server.broadcast(
+          "chatroom_#{@chatroom.id}_user_#{member.id}",
+          {
+            message_html: render_to_string(
+              partial: "messages/message",
+              locals: { message: @message, current_user: member }
+            )
+          }
+        )
+      end
+      head :ok
+    else
+      head :unprocessable_entity
+    end
   end
-end
 end
