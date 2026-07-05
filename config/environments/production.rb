7f436a0c87fcc1e5cab304604e8e83d56a2449dd
Usama <usama.zubair@snappymob.com>
2026-07-05 14:34:34 +0800

    url fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.default_options = {
     from: "TreeOfUs <support@tree-of-us.com"
   }
-  config.action_mailer.default_url_options = { host: "https://TreeOfUs-1-0a0u.onrender.com", protocol: "https" }
+  config.action_mailer.default_url_options = { host: "tree-of-us.com", protocol: "https" }
   config.action_mailer.delivery_method = :resend
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :resend
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "tree-of-us.com" }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
