950e50d4eee649ef29e078513d5d83759ce963c2
Usama <usama.zubair@snappymob.com>
2026-07-05 17:25:07 +0800

    fixing messages view

--- config/application.rb
+++ config/application.rb
@@ -16,16 +16,7 @@ module TreeOfUs
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-  end
-  config.generators do |g|
+    config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
         view_specs: false,
@@ -35,4 +26,14 @@ module TreeOfUs
         request_specs: true
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+
 end
