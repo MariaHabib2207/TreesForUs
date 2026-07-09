413ce52417626df94321b1f8abcf944b0be489f1
Usama <usama.zubair@snappymob.com>
2026-07-09 09:00:29 +0800

    missed calls notifications

--- app/notifiers/missed_call_notifier.rb
+++ app/notifiers/missed_call_notifier.rb
@@ -1,5 +1,16 @@
 class MissedCallNotifier < Noticed::Event
   deliver_by :database
+  deliver_by :action_cable do |config|
+    config.channel = "NotificationChannel"
+    config.stream = -> { recipient }
+    config.message = -> {
+      {
+        title: "Missed call",
+        body: "#{params[:caller].full_name} tried to call you",
+        url: chatroom_path(params[:chatroom])
+      }
+    }
+  end
 
   param :chatroom
   param :caller
